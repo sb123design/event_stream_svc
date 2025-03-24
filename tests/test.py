c3374129a35e6e168a843adee21a66f013675af0
sb123design <tentanium2@gmail.com>
2025-03-23 19:07:33 -0700

    add comments for what tests would do

---
+++ tests/test.py
@@ -0,0 +1,7 @@
+# Logic to test the endpoints and utilty functions go here, ran out of time.
+
+# Pytest test cases
+
+# Add dummy data through the pytest run, then can assert that the history endpoint correctly returns the last N events.
+# Add test for creating data
+# Add test for the stream endpoint output and time interval logic
